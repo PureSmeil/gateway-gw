e3d65fad99ef67929493033dbfe16a68e80ee794
ZR19090090-张婉雪 <zhangwanxue@zhongruigroup.com>
2020-07-31 13:59:51 +0800

    第一次提交

---
+++ src/main/java/puresmile/gatewaygw/GatewayGwApplication.java
@@ -0,0 +1,13 @@
+package puresmile.gatewaygw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayGwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayGwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/puresmile/gatewaygw/GatewayGwApplicationTests.java
@@ -0,0 +1,13 @@
+package puresmile.gatewaygw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayGwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
